7f796121cf9007eaa0e17d2d0c9d42e8a91d0819
DandD888 <64314351+DandD888@users.noreply.github.com>
2020-06-18 13:58:01 +0300

    Add files via upload

---
+++ EKZAMEN/src/Exam/Indications.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VY
+{
+
+    class Indications : Object
+    {
+        private float t;
+        private string napV;
+        private int speed;
+        private int count;
+
+        public float T { get => t; set => t = value; }
+        public string NapV { get => napV; set => napV = value; }
+        public int Speed { get => speed; set => speed = value; }
+        public Int32 Count { get => count; set => count = value; }
+
+        public Indications(float t, string napV, int speed, int count)
+        {
+            this.T = t;
+            this.NapV = napV;
+            this.Speed = speed;
+            this.count = count;
+        }
+        public Indications()
+        {
+            this.T = 0;
+            this.NapV = "";
+            this.Speed = 0;
+        }
+        public static Indications[] Sort(Indications[] indications)
+        {
+            {
+
+                for (int i = 0; i < indications.Length; i++)
+                {
+                    for (int j = i + 1; j < indications.Length; j++)
+                    {
+                        if ( indications[i].speed < indications[j].speed)
+                        {
+                            var temp = indications[i].speed;
+                            indications[i].speed = indications[j].speed;
+                            indications[j].speed = temp;
+                        }
+                    }
+                }
+
+                return indications;
+            }
+        }
+
+        public static bool SaveAs(string path, Indications[] vet)
+        {
+            if (File.Exists(path.ToString()))
+            {
+
+                Console.WriteLine("Файл уже существует,перезаписать файл?(Y/N)");
+                string answer = Console.ReadLine();
+                if (answer == "y" || answer == "Y" || answer == "у" || answer == "У" || answer == "д" || answer == "Д")
+                {
+                    File.Delete(path);
+                    TextWriter opnFile = new StreamWriter(path);
+                    foreach (var vetr in vet)
+                    {
+                        opnFile.WriteLine(vet.ToString());
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                using (TextWriter opnFile = new StreamWriter(path.ToString()))
+                {
+                    foreach (var vetr in vet)
+                    {
+                        opnFile.WriteLine(vetr.ToString());
+                    }
+                    opnFile.Close();
+                }
+                return true;
+            }
+        }
+        public override string ToString()
+        {
+            return "Температура:" + T + " Напровление ветра:" + NapV + " Скорость ветра:" + Speed + "\n";
+        }
+
+    }
+}
---
+++ EKZAMEN/src/Exam/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VY
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите кол-во ветров:");
+                int count = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n");
+                Indications[] indications = new Indications[count];
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("Введите Температуру:");
+                    var t = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите Направление ветра(N,S,W,E):");
+                    var napV = Console.ReadLine();
+                    Console.WriteLine("Введите Скорость ветра");
+                    var speed = Convert.ToInt32(Console.ReadLine());
+                    indications[i] = new Indications(t, napV, speed, count);
+                }
+                indications = Indications.Sort(indications);
+                foreach (var indication in indications)
+                {
+                    Console.WriteLine(indications.ToString());
+                }
+                Console.WriteLine("Введите путь для сохранения файлов");
+                var path = Console.ReadLine();
+                path = "Text.txt";
+                Indications.SaveAs(path, indications);
+                Console.ReadLine();
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine(exe.Message);
+            }
+
+        }
+    }
+}
